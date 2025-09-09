0dc43bcc5e278e789a27e3c85829892ca0ba135a
Ahmad Zahran <Ahmad Zahran@LAPTOP-5D28A1OS>
2025-09-09 20:08:58 +0700

    Praktikum pekan 1

---
+++ laundryapps/src/ui/LoginFrame.java
@@ -0,0 +1,102 @@
+package ui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import model.User;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.SystemColor;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class LoginFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField txtUsername;
+	private JTextField txtPassword;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginFrame frame = new LoginFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoginFrame() {
+		setBackground(SystemColor.activeCaption);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 485, 470);
+		contentPane = new JPanel();
+		contentPane.setBackground(SystemColor.activeCaption);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtUsername = new JTextField();
+		txtUsername.setBounds(126, 153, 225, 38);
+		contentPane.add(txtUsername);
+		txtUsername.setColumns(10);
+		
+		txtPassword = new JTextField();
+		txtPassword.setColumns(10);
+		txtPassword.setBounds(126, 245, 225, 38);
+		contentPane.add(txtPassword);
+		
+		JButton btnLogin = new JButton("New button");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (User.login(txtUsername.getText(), txtPassword.getText())) {
+					new MainFrame().setVisible(true);
+					dispose();	
+				}else {
+					JOptionPane.ShowMessageDialog(null, "Login Gagal");
+				}
+			}
+		});
+		btnLogin.setBounds(150, 323, 178, 38);
+		contentPane.add(btnLogin);
+		
+		JLabel lblNewLabel = new JLabel("LaundryDisini");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Yu Gothic UI Semilight", Font.BOLD, 14));
+		lblNewLabel.setBounds(159, 10, 151, 47);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Username");
+		lblNewLabel_1.setBounds(126, 139, 73, 13);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Password");
+		lblNewLabel_2.setBounds(126, 230, 45, 13);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("“Bersih, Wangi, Rapi  Hidup Jadi Easy!”");
+		lblNewLabel_3.setFont(new Font("Goudy Old Style", Font.BOLD, 18));
+		lblNewLabel_3.setBounds(91, 74, 315, 30);
+		contentPane.add(lblNewLabel_3);
+	}
+}
---
+++ laundryapps/src/ui/MainFrame.java
@@ -0,0 +1,72 @@
+package ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+public class MainFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 586, 408);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnPesanan = new JButton("PESANAN");
+		btnPesanan.setBounds(53, 104, 118, 47);
+		contentPane.add(btnPesanan);
+		
+		JButton btnPengguna = new JButton("PENGGUNA");
+		btnPengguna.setBounds(53, 192, 118, 40);
+		contentPane.add(btnPengguna);
+		
+		JButton btnLayanan = new JButton("LAYANAN");
+		btnLayanan.setBounds(235, 104, 105, 47);
+		contentPane.add(btnLayanan);
+		
+		JButton btnLaporan = new JButton("LAPORAN");
+		btnLaporan.setBounds(235, 192, 105, 40);
+		contentPane.add(btnLaporan);
+		
+		JButton btnPelanggan = new JButton("PELANGGAN");
+		btnPelanggan.setBounds(407, 104, 97, 47);
+		contentPane.add(btnPelanggan);
+		
+		JButton btnProfile = new JButton("PROFILE");
+		btnProfile.setBounds(407, 192, 97, 40);
+		contentPane.add(btnProfile);
+		
+		JButton btnKeluar = new JButton("KELUAR");
+		btnKeluar.setBounds(168, 289, 238, 40);
+		contentPane.add(btnKeluar);
+	}
+
+}
