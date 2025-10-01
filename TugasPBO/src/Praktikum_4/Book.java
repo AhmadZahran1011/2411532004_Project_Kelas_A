9223e3995e5b072abecb82ae6b9a984ee33ac4a5
Ahmad Zahran <Ahmad Zahran@LAPTOP-5D28A1OS>
2025-10-01 08:27:30 +0700

    Praktikum 4

---
+++ TugasPBO/src/Praktikum_4/Book.java
@@ -0,0 +1,29 @@
+package Praktikum_4;
+
+public class Book {
+	private String title;
+	private String author;
+	private boolean isAvailable;
+	
+	public Book(String title, String author) {
+		this.title = title;
+		this.author = author;
+		this.isAvailable = true;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+	public String getTitle() { return title; }
+	public String getAuthor() { return author; }
+	public boolean isAvailable() { return isAvailable; }
+
+	public void borrowBook() { this.isAvailable = false; }
+	public void returnBook() { this.isAvailable = true; }
+}
+
